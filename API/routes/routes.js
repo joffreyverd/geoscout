b51fea5e5d4b9497fb85f4561ed4a9a5120abaa6
Joffrey Verd <jverd@protonmail.com>
2019-02-07 12:36:22 +0100

    Correctif mineur

--- API/routes/routes.js
+++ API/routes/routes.js
@@ -9,7 +9,7 @@ module.exports = function(app)
 	app.route('/user').get(user.listUser);
 	app.route('/friends').get(user.listRelations);
     app.route('/create-user').post(user.createUser);
-    app.route('/add-relation').put(user.askRelation);
+    app.route('/ask-relation').put(user.askRelation);
     app.route('/circuit').get(circuit.circuit);
     app.route('/step').get(step.step);
     app.route('/transit').get(transit.transit);
