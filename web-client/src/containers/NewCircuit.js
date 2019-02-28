afc897be58cd0945b9fae30bd4cd9c5afccde31a
Thomas Unterfinger <thomas.unterfinger@etu.unistra.fr>
2019-02-28 16:05:27 +0100

    Redimension de la  map

--- web-client/src/containers/NewCircuit.js
+++ web-client/src/containers/NewCircuit.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import '../css/app.css';
+import NewMap from '../components/NewMap';
 
 export default class NewCircuit extends Component {
 
     render() {
-        console.log(this.state);
+        
         return (
             <>
-                <h1>Créer un nouveau circuit :)</h1>
+                <NewMap />
             </>
         );
     }
