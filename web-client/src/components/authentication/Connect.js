ecd4e8a06919f69ab3b728f76a1a1171368cae72
Thomas <thomas.unterfinger@etu.unistra.fr>
2019-03-06 11:45:34 +0100

    Connexion : Gestion du form dans le state + fonction submit des données

--- web-client/src/components/authentication/Connect.js
+++ web-client/src/components/authentication/Connect.js
@@ -2,19 +2,36 @@ import React, { Component } from 'react';
 import { Button, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
 import { withRouter } from 'react-router-dom';
 
+import api from '../../utils/httpMethods';
+
 class Connect extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             email: '',
-            password: '',
-            modal: props.modal,
-            displayModal: props.displayModal
+            password: ''
         };
     }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit = () => {
+        api.post('signin', this.state).then((data) => {
+            //gestion du token
+            this.props.displayModal();
+        }).catch((error) => {
+            // Gestion d'erreur
+        });
+    }
+
     render() {
+        const { email, password } = this.state;
+
         return (
             <>
                 <ModalBody>
@@ -24,14 +41,18 @@ class Connect extends Component {
                             <Input
                                 type="email"
                                 name="email"
-                                placeholder="Saisissez votre email" />
+                                placeholder="Saisissez votre email"
+                                value={ email }
+                                onChange={ this.handleChange } />
                         </FormGroup>
                         <FormGroup>
                             <Label>Mot de passe</Label>
                             <Input
                                 type="password"
                                 name="password"
-                                placeholder="Saisissez votre mot de passe" />
+                                placeholder="Saisissez votre mot de passe"
+                                value={ password }
+                                onChange={ this.handleChange } />
                         </FormGroup>
                     </Form>
                 </ModalBody>
@@ -39,10 +60,10 @@ class Connect extends Component {
                 <ModalFooter>
                     <Button
                         color="primary"
-                        onClick={this.displayModal}>Connexion</Button>
+                        onClick={this.props.displayModal}>Connexion</Button>
                     <Button
                         color="secondary"
-                        onClick={this.displayModal}>Annuler</Button>
+                        onClick={this.prosp.displayModal}>Annuler</Button>
                 </ModalFooter>
             </>
         );
