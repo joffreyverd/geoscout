910ff83281ac440e2b422a13140f9359a7a8c308
Thomas Unterfinger <thomas.unterfinger@etu.unistra.fr>
2019-03-14 15:45:57 +0100

    Fonction de drag & drop des étapes

--- web-client/src/components/step/StepListItem.js
+++ web-client/src/components/step/StepListItem.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default ({ step, onClickItem }) => (
     <li className='step-wrapper-item' draggable
         onClick={() => onClickItem(step)}
-        onDragOver={(e) => { e.preventDefault(); console.log(step) }}>
+        onDragStart={(e) => handleDragStart(e, step)}
+        onDragOver={(e) => { e.preventDefault(); }}
+        onDrop={(e) => handleDrop(e, step.order)}>
 
         <div>
             <span className='step-name'>{step.name}</span>
@@ -17,3 +19,26 @@ export default ({ step, onClickItem }) => (
 
     </li>
 );
+
+/**
+ * 
+ * @param {Event} event 
+ * @param {Step} step 
+ */
+function handleDragStart(event, step) {
+    event.dataTransfer.setData('id', step.id_step);
+    event.dataTransfer.setData('order', step.order)
+}
+
+/**
+ * 
+ * @param {Event} event 
+ * @param {Integer} newOrder 
+ */
+function handleDrop(event, newOrder) {
+    let id = event.dataTransfer.getData('id');
+    let oldOrder = event.dataTransfer.getData('order');
+    console.log('id : ' + id);
+    console.log('old : ' + oldOrder);
+    console.log('new : ' + newOrder);
+}
