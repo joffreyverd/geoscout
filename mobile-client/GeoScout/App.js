aa2f855878e0b3ea0519266d7a627d452201638d
Stevy <stevy.palarski@gmail.com>
2019-03-15 11:09:54 +0100

    Client-Mobile: changement de l'arboresence fichierr + ajout page sign v1

--- mobile-client/GeoScout/App.js
+++ mobile-client/GeoScout/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import GeoLocation from './Location';
+import Sign from './src/components/Sign';
 
 export default class App extends React.Component {
   render() {
     return (
-      <GeoLocation/>
+      <Sign/>
     );
   }
 }
