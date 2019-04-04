ca4af399d022aac22bf5ea7ecea6e81def5c810c
Stevy <stevy.palarski@gmail.com>
2019-04-04 18:16:02 +0200

    Ajout format screen Etape Transit Question Start

--- mobile-client/GeoScout/src/screens/Play/Transit.js
+++ mobile-client/GeoScout/src/screens/Play/Transit.js
@@ -4,9 +4,13 @@ import {
     Text,
     TouchableOpacity,
     AsyncStorage,
-    Alert
+    Alert,
+    StyleSheet,
+    ScrollView,
+    Dimensions
 } from  'react-native';
 import { Location, TaskManager } from 'expo';
+import HTML from 'react-native-render-html';
 
 // Nom de la variable dans AsyncStorage 
 const DETECTED = 'stepDetected';
@@ -82,15 +86,19 @@ class Transit extends React.Component {
         const step = circuit.Steps[stepNumber];
 
         return (
-            <View>
-                <Text>Transit vers l'étape {step.order}</Text>
-                <Text>{step.instruction}</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>Transit vers l'étape {step.order}</Text>
+                <ScrollView style={{ flex: 1 }}>
+                    <HTML html={step.instruction} imagesMaxWidth={Dimensions.get('window').width} />
+                </ScrollView>
 
                 {step.validation && 
                     <TouchableOpacity
                         onPress={() => navigate('Etape', { circuit, step: stepNumber })}
+                        activeOpacity={0.8}
+                        style={styles.button}
                     >
-                        <Text>Je suis arrivé</Text>
+                        <Text style={styles.textButton}>Je suis arrivé</Text>
                     </TouchableOpacity>
                 }
             </View>
@@ -113,3 +121,29 @@ TaskManager.defineTask(DETECT_STEP, ({ data: { eventType, region }, error }) => 
 })
 
 export default Transit;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    title: {
+        color: '#2c3e50',
+        fontSize: 24,
+        marginTop: 30,
+        marginBottom: 15,
+        fontWeight: 'bold'
+    },
+    button: {
+        backgroundColor: '#2c3e50',
+        borderRadius: 5,
+        padding: 8,
+        marginBottom: 5,
+        width: '80%',
+        alignItems: 'center'
+    },
+    textButton: {
+        color: '#fff',
+        fontSize: 18
+    }
+});
