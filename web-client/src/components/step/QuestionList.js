d99d57927469a113af6af1f18cea3732481f9bb2
joffreyverd <jverd@protonmail.com>
2019-04-04 12:30:23 +0200

    Get différentes questions

--- web-client/src/components/step/QuestionList.js
+++ web-client/src/components/step/QuestionList.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import { TabContent } from 'reactstrap';
-import QuestionListItem from './QuestionListItem';
+import { FormGroup, Label, Input, TabPane, Row, Col, TabContent } from 'reactstrap';
+import 'antd/dist/antd.css';
 
-export default ({ items = [], handleChangeQuestion }) => (
+export default class QuestionList extends React.Component {
 
-    <TabContent activeTab='1'>
-        {
-            items.map(item => <QuestionListItem
-                {...item}
-                handleChangeQuestion={handleChangeQuestion}
-            />)
-        }
-    </TabContent>
+    state = {
+    }
 
-);
+    render() {
+        const { activeTab, handleChangeQuestion } = this.props;
+        const { wording, response } = (this.props.items[activeTab]) ? this.props.items[activeTab] : '';
+
+        return (
+
+            <>
+                <TabContent activeTab={activeTab}>
+                    <TabPane tabId={activeTab}>
+                        <Row>
+                            <Col sm='12'>
+
+                                <FormGroup>
+                                    <Label>Intitulé de la question</Label>
+                                    <Input
+                                        type='textarea'
+                                        name='wording'
+                                        value={(wording !== undefined) ? wording : ''}
+                                        onChange={handleChangeQuestion}
+                                    />
+                                </FormGroup>
+
+                                <FormGroup>
+                                    <Label>Réponse</Label>
+                                    <Input
+                                        type='textarea'
+                                        name='response'
+                                        value={(response !== undefined) ? response : ''}
+                                        onChange={handleChangeQuestion}
+                                    />
+                                </FormGroup>
+
+                            </Col>
+                        </Row>
+                    </TabPane>
+                </TabContent>
+            </>
+        );
+
+    }
+
+}
