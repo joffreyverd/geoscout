be37eab0fa2727d8a731032b63e0c68a1de890b8
Quentin ANTONY <quentin.antony@etu.unistra.fr>
2019-06-16 10:52:34 +0200

    correction de bugs sur certaines routes

--- API/tests/testServeur.js
+++ API/tests/testServeur.js
@@ -3,34 +3,58 @@
 /* eslint-disable no-undef */
 /* eslint-disable indent */
 var request = require('supertest');
-describe('loading express', function () 
+module.exports =
 {
-		var server = null;
-		this.beforeAll(function () 
-		{
-			server = require('../serveur');
-			console.log('--------------------------- Demmarage serveur ---------------------------');
-		});
+	test :describe('loading express', function () 
+	{
+			var server = null;
+			this.beforeAll(function () 
+			{
+				server = require('../serveur');
+				console.log('--------------------------- Demmarage serveur ---------------------------');
+			});
 
-		this.afterAll(function () 
-		{
-			require('../serveur').close();
-			console.log('--------------------------- Arret serveur ---------------------------');
-		});
+			this.afterAll(function () 
+			{
+				require('../serveur').close();
+				console.log('--------------------------- Arret serveur ---------------------------');
+			});
 
-		/////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////
 
-		it('Chargement serveur à la racine \'/\'', function testSlash(done) 
-		{
-			request(server)
-			.get('/')
-			.expect(200, done);
-		});
-		
-		it('Acces mauvaise route', function testPath(done) 
-		{
-			request(server)
-			.get('/test')
-			.expect(404, done);
-		});
-});
+			it('Chargement serveur à la racine \'/\'', function testSlash(done) 
+			{
+				request(server)
+				.get('/')
+				.expect(200, done);
+			});
+			
+			it('Acces mauvaise route', function testPath(done) 
+			{
+				request(server)
+				.get('/test')
+				.expect(404, done);
+			});
+
+			it('login', function testPath(done) 
+			{
+				let data = 
+				{
+					email : 'antony.quent1@gmail.com',
+					password : 'abcdefgh45'
+				};
+
+				let body = { firstname: 'Quentin', lastname: 'ANTONY', id_user: 37 };
+
+				request(server)
+				.post('/signin')
+				.send(data)
+				.expect(200)
+				.then(response =>
+				{
+					assert(response.body.User,body);
+				});
+				done();
+			});
+	})
+};
